0929983b192418cd848215cc2015f3560c489a6f
Gabriel Alejandro <113558132+PPAGabriel@users.noreply.github.com>
2023-05-23 09:41:45 +0200

    Modificacion del paquete Model (#8)

--- src/Model/MHospital.java
+++ src/Model/MHospital.java
@@ -1,8 +1,11 @@
 package Model;
 
-import Clases.Hospital;
 import java.util.ArrayList;
 
-public class MHospital extends GestionBases{
+public class MHospital{
+    GestionBases auxCon= new GestionBases();
 
+    public void crearHospital(String codH,String nombreH,String tipoH,int nroMedico, int nroHabit){
+        auxCon.conectar();
+    }
 }
