5576108d2a570e564a65ca1d70eef6b99824b833
Nuevo Nombre <gperezpereira@danielcastelao.org>
2023-05-24 12:00:32 +0200

    Modificacion MMedico y MPaciente

--- src/Model/MMedico.java
+++ src/Model/MMedico.java
@@ -4,12 +4,14 @@ import java.awt.*;
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import Clases.Medico;
+import com.nicosteuerberg.datos.VentanaLabel;
 
 import javax.swing.*;
 import java.sql.*;
 
 public class MMedico{
     GestionBases auxCon= new GestionBases();
+    int verificacion;
 
     /**
      * Crear Médico en la base de Datos
@@ -28,14 +30,49 @@ public class MMedico{
             ps.setString(3, codH);
 
             ps.executeUpdate();
-            label.setForeground(Color.black);
-            label.setText("Médico añadido!");
+
+                VentanaLabel.mensajeLabel("Médico creado", label, Color.black);
 
         } catch (SQLException e) {
-            label.setText("ERROR: insert fallido");
-            label.setForeground(Color.red);
+            VentanaLabel.mensajeLabel("ERROR a la hora de hacer el insert",label,Color.red);
         }
 
+    }
+
+    public void modificarMedico(String codM, String nombreM,String codH, JLabel label){
+        try {
+            Connection con = auxCon.conectar();
+            PreparedStatement ps = con.prepareStatement("UPDATE medico SET nomM=?,codH=? WHERE codM=?");
+
+            ps.setString(1, nombreM);
+            ps.setString(2, codH);
+            ps.setString(3, codM);
+
+            verificacion=ps.executeUpdate();
+            if(verificacion==0){
+                VentanaLabel.mensajeLabel("No existe el medico con el codigo "+codM,label,Color.red);
+            }else {
+                VentanaLabel.mensajeLabel("Medico modificado", label, Color.black);
+            }
+
+        }catch(SQLException e){
+            VentanaLabel.mensajeLabel("ERROR en la modificación del medico",label,Color.red);
+        }
+    }
+
+    public void eliminarMedico(String codM, JLabel label){
+        try {
+            Connection con = auxCon.conectar();
+            PreparedStatement ps = con.prepareStatement("DELETE FROM medico WHERE codM=?");
+
+            ps.setString(1, codM);
+
+            ps.executeUpdate();
+            VentanaLabel.mensajeLabel("Médico eliminado",label,Color.black);
+
+        }catch(SQLException e){
+            VentanaLabel.mensajeLabel("Error en la eliminación del medico",label,Color.red);
+        }
     }
 
 }
