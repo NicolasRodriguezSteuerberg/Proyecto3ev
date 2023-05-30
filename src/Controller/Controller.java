15fe529157971dce31de2029a815fe9ced7f38c6
Gabriel Alejandro <113558132+PPAGabriel@users.noreply.github.com>
2023-05-30 14:15:16 +0200

    Modificacion de los Model y añadimos métodos en el Controller (#20)
    * Modificacion del paquete Model
    
    * Creacion de métodos CrearMedico y ContarMedico
    
    * cambios innecesarios
    
    * Modificacion MMedico y MPaciente
    
    * creacion de mmetodo contarMedicos()
    
    * Modificacion de los Model y añadimos los mÃetodos en el Controller
    
    ---------
    
    Co-authored-by: Nicolás Rodríguez Steuerberg <113533483+NicolasRodriguezSteuerberg@users.noreply.github.com>

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -35,9 +35,6 @@ public class Controller {
      * @param label -> etiqueta de la interfaz para mostrar mensajes
      */
     static void crearHospital(String codH, String nombreH, String tipoH, int nroHabitaciones, JLabel label){
-        /**ToDo
-         * Contar medicos de un hospital
-         */
         int nroMedico = obxM.contarMedicos(codH,label);
         lHospital.add(new Hospital(codH, nombreH, tipoH, nroMedico, nroHabitaciones));
         obxH.crearHospital(lHospital, label);
@@ -65,4 +62,67 @@ public class Controller {
         obxH.eliminarHospital(lHospital, codH,label);
     }
 
+    /**
+     *Crear médico
+     * @param codM -> código del médico
+     * @param nomM -> nombre del médico
+     * @param codH -> código del hospital donde trabaja el médico
+     */
+    static void crearMedico(String codM, String nomM,String codH){
+        lMedico.add(new Medico(codM,nomM,codH));
+
+    }
+
+    /**
+     * Modificar Médico
+     * @param codM -> código del médico
+     * @param nomM -> nombre del médico
+     * @param codH -> codigo del hospital donde pertenece
+     * @param label -> etiqueta de la interfaz para mostrar mensajes
+     */
+    static void modificarMedico(String codM,String nomM,String codH, JLabel label){
+        obxM.modificarArray(lMedico,codM,nomM,codH);
+        obxM.modificarMedico(lMedico,codM,label);
+    }
+
+    /**
+     * Borrar médico
+     * @param codM -> codigo del medico
+     * @param label -> etiqueta de la interfaz para mostrar mensajes
+     */
+    static void eliminarMedico(String codM, JLabel label){
+        obxH.eliminarHospital(lHospital, codM,label);
+    }
+
+    /**
+     * Crear Paciente
+     * @param codP -> código del paciente
+     * @param nomP -> nombre del paciente
+     * @param codM -> código del médico que lo atiende
+     */
+    static void crearPaciente(String codP, String nomP,String codM){
+        lPaciente.add(new Paciente(codP,nomP,codM));
+
+    }
+
+    /**
+     * Modificar Paciente
+     * @param codP -> código del paciente
+     * @param nomP -> nombre del paciente
+     * @param codM -> codigo del médico que lo atiende
+     * @param label -> etiqueta de la interfaz para mostrar mensajes
+     */
+    static void modificarPaciente(String codP,String nomP,String codM, JLabel label){
+        obxP.modificarArray(lPaciente,codP,nomP,codM);
+        obxP.modificarPaciente(lPaciente,codP,label);
+    }
+
+    /**
+     * Borrar paciente
+     * @param codP -> codigo del paciente
+     * @param label -> etiqueta de la interfaz para mostrar mensajes
+     */
+    static void eliminarPaciente(String codP, JLabel label){
+        obxP.eliminarPaciente(lPaciente, codP,label);
+    }
 }
