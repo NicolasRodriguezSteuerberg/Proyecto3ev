42d9a1e42993fef303eb7fbe08bf61ae892d6dc6
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-05-31 13:47:33 +0200

    mejorada la vista

--- src/View/Vista.java
+++ src/View/Vista.java
@@ -13,25 +13,53 @@ public class Vista {
 
     public void crearPaneles(){
         pMenu = ventana.pMenu;
+
         pHospital = new pHospital();
+        pHospital.setBounds(0, 0, 1280, 720);
+        ventana.add(pHospital);
+        pHospital.setVisible(false);
+
         pMedico = new pMedico();
+        pMedico.setBounds(0, 0, 1280, 720);
+        ventana.add(pMedico);
+        pMedico.setVisible(false);
+
         pPaciente = new pPaciente();
+        pPaciente.setBounds(0, 0, 1280, 720);
+        ventana.add(pPaciente);
+        pPaciente.setVisible(false);
+
+
+    }
+
+    public void quitarPaneles(){
+        pMenu.setVisible(false);
+        pHospital.setVisible(false);
+        pMedico.setVisible(false);
+        pPaciente.setVisible(false);
     }
 
     public void visualizarMenu(){
         ventana.setBounds(0,0,400,300);
+        quitarPaneles();
         pMenu.setVisible(true);
     }
 
     public void visualizarHospital(){
-
+        ventana.setBounds(0,0,1280,720);
+        quitarPaneles();
+        pHospital.setVisible(true);
     }
 
     public void visualizarMedico(){
-
+        ventana.setBounds(0,0,1280,720);
+        quitarPaneles();
+        pMedico.setVisible(true);
     }
 
     public void visualizarPaciente(){
-
+        ventana.setBounds(0,0,1280,720);
+        quitarPaneles();
+        pPaciente.setVisible(true);
     }
 }
